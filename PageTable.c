c1add830c9f1fa41cc71b0bc668e596d9ea97c33
Toby Huang <619058900@qq.com>
2017-10-05 21:15:28 +1100

    finish all the content of assignment2

--- PageTable.c
+++ PageTable.c
@@ -24,7 +24,9 @@ typedef struct {
    int  loadTime;    // clock tick for last time loaded
    int  nPeeks;      // total number times this page read
    int  nPokes;      // total number times this page modified
-   // TODO: add more fields here, if needed ...
+   // field for clock swipe
+   int referenced;
+
 } PTE;
 
 // The virtual address space of the process is managed
@@ -37,6 +39,95 @@ static int  nPages;         // # entries in page table
 static int  replacePolicy;  // how to do page replacement
 static int  fifoList;       // index of first PTE in FIFO list
 static int  fifoLast;       // index of last PTE in FIFO list
+
+// The double link list is for queing of fifo or lru
+typedef struct dll_node_t {
+    // record the page number
+    int pno;
+    // proint to the previou node
+    struct dll_node_t *prev;
+    // proint to the next node
+    struct dll_node_t *next;
+} *dll_n;
+
+typedef struct dll_tree_t {
+    // it's a circle dll, so the hand -> prev is the tail
+    dll_n hand;
+} *dll;
+
+
+// for the queuing of access
+dll queue;
+
+dll_n newNode(int pno){
+    // create of a new node
+    dll_n this = malloc(sizeof(struct dll_node_t));
+    if (this == NULL) {
+        /* node create error */
+        fprintf(stderr, "Couldn't malloc for queue node\n" );
+        exit(EXIT_FAILURE);
+    }
+    // init its val
+    this->prev = this->next = NULL;
+    this->pno = pno;
+    return this;
+}
+
+// find the node with pageNo in the queue
+dll_n findNode(int pno){
+    if (queue->hand->pno == pno) {
+        /* queue->hand is the node require */
+        return queue->hand;
+    }
+    for (dll_n this_node = queue->hand->next; this_node != queue->hand;
+         this_node = this_node->next) {
+         /* look throught the whole array */
+         if (this_node->pno == pno) {
+             /* this_node is required */
+             return this_node;
+         }
+    }
+    // Couldn't find the node
+    return NULL;
+}
+
+//  functions for manage the queue
+void pushQueue(int pno){
+    // push the record in queue by provide pageNo
+    if (queue->hand == NULL) {
+        /* this is the first node */
+        dll_n this_node = newNode(pno);
+        // arrange the link
+        this_node->prev = this_node->next = this_node;
+        // put this node into queue
+        queue->hand = this_node;
+        // end this function
+        return;
+    }
+    // normal pushing
+    dll_n this_node = findNode(pno);
+    if (this_node == NULL) {
+        /* this page has note been accessed */
+        this_node = newNode(pno);
+    }
+    else {
+        // push back this node to the tail to of the queue
+        // stitching the gap, caused by the leaving of this_node
+        this_node->prev->next = this_node->next;
+        this_node->next->prev = this_node->prev;
+
+    }
+    // push this_node at tail
+    this_node->prev = queue->hand->prev;
+    queue->hand->prev->next = this_node;
+    queue->hand->prev = this_node;
+    this_node->next = queue->hand;
+}
+
+
+
+
+
 
 // Forward refs for private functions
 
@@ -63,7 +154,18 @@ void initPageTable(int policy, int np)
       p->accessTime = NONE;
       p->loadTime = NONE;
       p->nPeeks = p->nPokes = 0;
+      // flag for clock swipe
+      p->referenced = 0;
    }
+   // initial the queue
+   queue = malloc(sizeof(struct dll_tree_t));
+   if (queue == NULL) {
+       /* couldn't allocate memory for queue */
+       fprintf(stderr, "Couldn't malloc memory for queue\n" );
+       exit(EXIT_FAILURE);
+   }
+   queue->hand = NULL;
+
 }
 
 // requestPage: request access to page pno in mode
@@ -103,6 +205,8 @@ int requestPage(int pno, char mode, int time)
          PageTable[vno].status = ON_DISK;
          // - no longer modified
          PageTable[vno].modified = 0;
+         // - no longer referenced
+         PageTable[vno].referenced = 0;
          // - no frame mapping
          PageTable[vno].frame = NONE;
          // - not accessed, not loaded
@@ -121,6 +225,13 @@ int requestPage(int pno, char mode, int time)
       p->frame = fno;
       // - just loaded
       p->loadTime = time;
+
+      // record in queue for FIFO
+      if (replacePolicy == REPL_FIFO) {
+          pushQueue(pno);
+      }
+
+
       break;
    case IN_MEMORY:
       // add stats collection
@@ -137,6 +248,12 @@ int requestPage(int pno, char mode, int time)
       p->modified = 1;
    }
    p->accessTime = time;
+   // record the info require by LRU or Clock
+   if (replacePolicy == REPL_LRU || replacePolicy == REPL_CLOCK) {
+       pushQueue(pno);
+       p->referenced = 1;
+   }
+
    return p->frame;
 }
 
@@ -145,38 +262,73 @@ int requestPage(int pno, char mode, int time)
 
 static int findVictim(int time)
 {
-   int victim = NONE;
-   // record the oldest time
-   int oldest = time;
-   switch (replacePolicy) {
-   case REPL_LRU:
-      // implement LRU strategy
-      for (int i = 0; i < nPages; i++) {
-          /* go throught all the page for the oldest page */
-          if (PageTable[i].accessTime != NONE && PageTable[i].accessTime < oldest ) {
-              /* replace oldest and the victim */
-              victim = i;
-              oldest = PageTable[i].accessTime;
-          }
-      }
+    int victim = NONE;
+    // record the node would be delete
+    dll_n this_node = NULL;
+
+    switch (replacePolicy) {
+        case REPL_LRU:
+        // implement LRU strategy
+        case REPL_FIFO:
+            //  implement FIFO strategy
+
+            //  return the tail of the queue;
+            victim = queue->hand->pno;
+
+            // free the memory used by node record to prevent memory leak
+            this_node = queue->hand;
+            //stitching the gap, caused by the leaving of this_node
+            if (this_node->prev == this_node) {
+            /* the queue only have this_node */
+                queue->hand = NULL;
+            }
+            else{
+                // rearrange the link
+                this_node->prev->next = this_node->next;
+                this_node->next->prev = this_node->prev;
+                // assign the new tail for the queue
+                queue->hand = this_node->next;
+            }
+            // free the memory
+            free(this_node);
 
 
-      break;
-   case REPL_FIFO:
-      //  implement FIFO strategy
-      for (int i = 0; i < nPages; i++) {
-          /* go throught all the page for the oldest page */
-          if (PageTable[i].loadTime != NONE && PageTable[i].loadTime < oldest ) {
-              /* replace oldest and the victim */
-              victim = i;
-              oldest = PageTable[i].loadTime;
-          }
-      }
-      break;
-   case REPL_CLOCK:
-      return 0;
-   }
-   return victim;
+            break;
+        case REPL_CLOCK:
+            // iterate the hand to find the referenced is 0
+            while (PageTable[queue->hand->pno].referenced !=0) {
+                /* iterate the queue */
+                // reset the referenced flag
+                PageTable[queue->hand->pno].referenced = 0;
+                // go to next node that recorded
+                queue->hand = queue->hand->next;
+            }
+
+
+            //  return the tail of the queue;
+            victim = queue->hand->pno;
+
+            // free the memory used by node record to prevent memory leak
+            this_node = queue->hand;
+            //stitching the gap, caused by the leaving of this_node
+            if (this_node->prev == this_node) {
+                /* the queue only have this_node */
+                queue->hand = NULL;
+            }
+            else{
+                // rearrange the link
+                this_node->prev->next = this_node->next;
+                this_node->next->prev = this_node->prev;
+                // assign the new tail for the queue
+                queue->hand = this_node->next;
+            }
+            // free the memory
+            free(this_node);
+
+            break;
+
+    }
+    return victim;
 }
 
 // showPageTableStatus: dump page table
